ef9a1ae44daa64918b83e4811c4b0df97c5dc635
Artem Subbotin <subbotin.artem@gmail.com>
2022-05-13 12:46:24 +0300

    deployed implementation page is completed

--- HTML5Application/public_html/js/admin-tab.js
+++ HTML5Application/public_html/js/admin-tab.js
@@ -1,48 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function refreshDeployedImplementations(){
-
-    let list = localStorage.getItem("deployedImplementations");
-    list = (typeof(list) === 'undefined' || list == null) ? [] : JSON.parse(list);
-    $('#adminTabDeployedList').html('<table class="table"><tbody></tbody></table>');
-    
-    //Try to get tbody first with jquery children. works faster!
-    var tbody = $('#adminTabDeployedList table').children('tbody');
-    console.log(list);
-    if (list.length>0) {
-        for (let item of list) {
-            tbody.append('<tr><th>'+item.name+'</th><th>'+item.address+'</th></tr>');
-        }
-    } else {
-        tbody.append('<tr><th>There are no data</th></tr>');
-    }
-
-}
-
-function saveImplementation(name, address) {
-    let list = localStorage.getItem("deployedImplementations");
-    list = (typeof(list) === 'undefined' || list == null) ? [] : JSON.parse(list);
-    list.push({
-        name: name,
-        address: address
-    });
-    localStorage.setItem("deployedImplementations", JSON.stringify(list));
-    
-}
-
-
-    
-async function test() {
-    
-}
+var implementationStorageObj = new ContractStorage("deployedImplementations");
 
 $("#JsDeployedListRefresh").off("click").on("click", function(){
-    refreshDeployedImplementations();
+    implementationStorageObj.writeHtml("adminTabDeployedList");
+    //refreshDeployedImplementations();
 });
+$("#JsDeployedListTrash").off("click").on("click", function(){
+    if (window.confirm("Are you sure to clear list?")) {
+        implementationStorageObj.clear();
+        implementationStorageObj.writeHtml("adminTabDeployedList");
+    }
+    
+});
+
 $("#AdminPage-selectTemlate").off("change").on("change", function(){
     $(".AdminPage-templateContent").hide();
     $(".AdminPage-"+this.value).show();
@@ -51,36 +20,47 @@ $("#AdminPage-selectTemlate").off("change").on("change", function(){
 
 $('.adminPageContainer button').off("click").on("click", function(e){
     e.preventDefault();
-    let option = $("#AdminPage-selectTemlate").val();
+    
+    
     if (provider.selectedAddress != null) {
-        fetch('artifacts/'+option+'.json')
-            .then(response => response.text())
-            .then( async function(text){
-                text = JSON.parse(text);
-                let abi = text.abi;
-                let bytecode = text.bytecode;
-                
-                const web3provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-                // get a signer wallet!
-                const signer = web3provider.getSigner();
+        let option = $("#AdminPage-selectTemlate").val();
+        let itemExists = implementationStorageObj.itemExists(option);
+        if (!itemExists || (itemExists && window.confirm("impementation already deployed. are you sure to replace exists?"))) {
 
-                // The factory we use for deploying contracts
-                factory = new ethers.ContractFactory(abi, bytecode, signer)
 
-                // Deploy an instance of the contract
-                contract = await factory.deploy();
+            fetch('artifacts/'+option+'.json')
+                .then(response => response.text())
+                .then( async function(text){
+                    text = JSON.parse(text);
+                    let abi = text.abi;
+                    let bytecode = text.bytecode;
 
-                saveImplementation(option, contract.address);
-                fetchAccountData();
-                refreshDeployedImplementations();
-//                console.log(provider.selectedAddress);
-//                const signer = await ethers.provider.getSigner(provider.selectedAddress)
-    //            signer.sendTransaction(...) // what does this do?
-            });
+                    const web3provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+                    // get a signer wallet!
+                    const signer = web3provider.getSigner();
+
+                    // The factory we use for deploying contracts
+                    factory = new ethers.ContractFactory(abi, bytecode, signer)
+
+                    // Deploy an instance of the contract
+                    contract = await factory.deploy();
+
+                    implementationStorageObj.setItem(option, contract.address);
+                    //saveImplementation(option, contract.address);
+                    fetchAccountData();
+                    implementationStorageObj.writeHtml("adminTabDeployedList");
+                    //refreshDeployedImplementations();
+    //                console.log(provider.selectedAddress);
+    //                const signer = await ethers.provider.getSigner(provider.selectedAddress)
+        //            signer.sendTransaction(...) // what does this do?
+                });
+
+        }
     }
 });
 
-refreshDeployedImplementations();
+implementationStorageObj.writeHtml("adminTabDeployedList");
+//refreshDeployedImplementations();
   // outputs the content of the text file
 
 //saveImplementation("lorem", 0x123);
